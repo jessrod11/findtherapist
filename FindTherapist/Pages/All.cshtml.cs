795d671b5d772a5b677b42d41c26d81d520e704f
Stix <jessica.rodgers11@gmail.com>
2019-02-09 13:43:17 -0600

    added api access via jquery to therapist razor page

--- FindTherapist/Pages/All.cshtml.cs
+++ FindTherapist/Pages/All.cshtml.cs
@@ -9,6 +9,21 @@ namespace FindTherapist.Pages
     public class AllModel : PageModel
     {
         public string Message { get; set; }
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Img { get; set; }
+        public string Specialty { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public int Zip { get; set; }
+        public long Phone { get; set; }
+        public string Web { get; set; }
+        public int UserId { get; set; }
+        public int SavedId { get; set; }
+        public int FavId { get; set; }
+        public int ReviewId { get; set; }
 
         public void OnGet()
         {
